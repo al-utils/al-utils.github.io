5930778d1e81348444d5f3b6a162d0d39851779e
Java <indead1124@gmail.com>
2023-05-25 00:45:41 -0400

    get all data with one api call...

--- airing/airing.js
+++ airing/airing.js
@@ -1,3 +1,5 @@
+const maxPages = 30
+
 if (userQuery !== '' && userQuery !== null) {
     if (userQuery.split(',').length > 1 || userQuery.split(' ').length > 1) {
         sendError('Too many users')
@@ -35,9 +37,33 @@ function newSearch() {
     window.open('/airing/?user=' + user, '_self')
 }
 
-// function airingChart() {
-
-// }
+function buildQuery(username) {
+    r = ''
+    for (i = 1; i < maxPages; i++) {
+        r += `
+        page${i}: Page(page: ${i}, perPage: 50) {
+            mediaList(userName: "${username}", status_in: [CURRENT, PLANNING], type: ANIME) {
+                media {
+                    id
+                    title {
+                        english
+                    }
+                    episodes
+                    coverImage {
+                        extraLarge
+                    }
+                    nextAiringEpisode {
+                        id
+                        timeUntilAiring
+                        episode
+                    }
+                }
+            }
+        }
+        `
+    }
+    return r
+}
 
 function search(user) {
 
@@ -48,7 +74,6 @@ function search(user) {
         if (loading.style.display == 'none') {
             return
         }
-        console.log('test')
         loading.textContent += '.'
         if (loading.textContent.length > 10) {
             loading.textContent = 'loading'
@@ -64,14 +89,9 @@ function search(user) {
             'Accept': 'application/json',
         },
         body: JSON.stringify({
-            query: `query ($name: String) {
-                        User (name: $name) {
-                            id
-                        }
-                    }`,
-            variables: {
-                name: user
-            }
+            query: `query {
+                        ${buildQuery(user)}
+                    }`
         })
     }
     fetch(url, options).then(handleResponse)
@@ -79,209 +99,80 @@ function search(user) {
         .catch((error) => {
             console.log(error)
             sendError('User not found: ' + user)
+            loading.style.display = 'none'
             return
         })
 
-    function pages() {
-        r = ''
-        for (i = 1; i < 50; i++) {
-            r += `
-            page${i}: Page(page: ${i}, perPage: 50) {
-                mediaList(userId: $id, status_in: [CURRENT, PLANNING], type: ANIME) {
-                    media {
-                        id
-                    }
-                }
-            }
-            `
-        }
-        return r
-    }
-
     function handleData(data) {
-        var id = data['data']['User']['id']
-    
-        // query to get planning and current lists
-        var options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({
-                query: `
-                query ($id: Int) {
-                    ${pages()}
-                }`,
-                variables: {
-                    id: id
-                }
-            })
-        }
-        fetch(url, options).then(handleResponse)
-            .then(handleData)
-            .catch((error) => {
-                console.log(error)
-                sendError("Didn't work (rate-limited?)")
-                return
-            })
+        let dict = {}
 
-        function handleData(data) {
-            mediaIds = []
-            for (i = 1; i < 50; i++) {
-                page = data['data']['page' + i]['mediaList']
-                // break when pages empty
-                if (page.length == 0) {
-                    break
-                }
-                // add ids to mediaIds
-                for (media of page) {
-                    mediaIds.push(media.media.id)
-                }
+        for (i = 1; i < maxPages; i++) {
+            let page = data['data']['page' + i]['mediaList']
+            // break when pages empty
+            if (page.length == 0) {
+                break
             }
-
-            function pages() {
-                r = ''
-                for (i = 1; i < 100; i++) {
-                    r += `
-                    page${i}: Page(page: ${i}, perPage: 50) {
-                        airingSchedules(mediaId_in: [${mediaIds}], notYetAired: true) {
-                            mediaId
-                            episode
-                            timeUntilAiring
-                        }
-                    }
-                    `
-                    // airingAt
-                }
-                return r
-            }
-
-            var options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                },
-                body: JSON.stringify({
-                    query: `
-                    query {
-                        ${pages()}
-                    }`
-                })
-            }
-            fetch(url, options).then(handleResponse)
-                .then(handleData)
-                .catch((error) => {
-                    console.log(error)
-                    sendError("Didn't work (rate-limited?)")
-                    return
-                })
-
-            function handleData(data) {
-                let seperate = {}
-                for (i = 1; i < 100; i++) {
-                    page = data['data']['page' + i]['airingSchedules']
-                    if (page.length == 0) {
-                        break
-                    }
-                    for (j = 0; j < page.length; j++) {
-                        animeId = page[j].mediaId
-                        key = '' + animeId
-                        if (seperate[key] == undefined) { // doesn't exist
-                            seperate[key] = {
-                                "episode": page[j].episode,
-                                "timeUntilAiring": page[j].timeUntilAiring
-                            }
-                        }
-                        // else if (seperate[key].episode > page[j].episode)
-                    }
-                }
-                // sort
-                keys = Object.keys(seperate)
-                keys.sort(function(x, y) {
-                    return seperate[x].timeUntilAiring - seperate[y].timeUntilAiring
-                })
-                
-                function allMedia(keys) {
-                    r = ''
-                    for (i = 0; i < keys.length; i++) {
-                        r += `
-                        a${keys[i]}: Media(id: ${keys[i]}) {
-                            title {
-                                english
-                            }
-                            episodes
-                            coverImage {
-                                extraLarge
-                            }
-                        }
-                        `
-                    }
-                    return r
-                }
-
-                var options = {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        query: `
-                        query {
-                            ${allMedia(keys)}
-                        }`
-                    })
-                }
-                fetch(url, options).then(handleResponse)
-                    .then(handleData)
-                    .catch((error) => {
-                        console.log(error)
-                        sendError("Didn't work (rate-limited?)")
-                        return
-                    })
-                
-                function handleData(data) {
-                    grid = document.getElementById('chart')
-                    data = data['data']
-                    Object.keys(data).forEach(function(key) {
-                        ele = document.createElement('a')
-                        let id = key.replace('a', '')
-                        ele.href = 'https://anilist.co/anime/' + id
-                        ele.target = '_blank'
-
-                        if (data[key].episodes == null)
-                            data[key].episodes = '?'
-                        
-                        ele.innerHTML = `
-                            <div class="chart-square" style="background-image: url('${data[key].coverImage['extraLarge']}')">
-                                <div class="gradient">
-                                    <p class="title">${data[key].title['english']}</p>
-                                    <div class="sub">
-                                        <p style="text-align: left">${secondsToString(seperate[id]['timeUntilAiring'])}</p>
-                                        <p style="text-align: right; filter: brightness(0.75)">${seperate[id]['episode']}/${data[key].episodes}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        `
-                        grid.appendChild(ele)
-                    });
-
-                    // hide loading
-                    loading.style.display = 'none'
-                }
-
-                function secondsToString(s) {
-                    let d = Math.floor(s / (3600 * 24))
-                    let h = Math.floor(s % (3600 * 24) / 3600)
-                    return `${d}d${h}h`
+            
+            for (j = page.length - 1; j >= 0; j--) {
+                let nextAiringEpisode = page[j]['media']['nextAiringEpisode']
+                if (nextAiringEpisode != null) {
+                    let anime = page[j]['media']
+                    let id = anime['id']
+                    let title = anime['title']['english']
+                    let totalEpisodes = anime['episodes']
+                    let coverImage = anime['coverImage']['extraLarge']
+                    let timeUntilAiring = anime['nextAiringEpisode']['timeUntilAiring']
+                    let nextEpisode = anime['nextAiringEpisode']['episode']
+                    dict['' + id] = {}
+                    dict['' + id]['title'] = title
+                    dict['' + id]['totalEpisodes'] = totalEpisodes
+                    dict['' + id]['coverImage'] = coverImage
+                    dict['' + id]['timeUntilAiring'] = timeUntilAiring
+                    dict['' + id]['nextEpisode'] = nextEpisode
                 }
             }
         }
+
+        // sort, getting list of ordered keys
+        keys = Object.keys(dict)
+        keys.sort(function(x, y) {
+            return dict[x].timeUntilAiring - dict[y].timeUntilAiring
+        })
+
+        // build grid tiles
+        let grid = document.getElementById('chart')
+        for (id of keys) {
+            ele = document.createElement('a')
+            ele.href = 'https://anilist.co/anime/' + id
+            ele.target = '_blank'
+
+            if (dict[id]['totalEpisodes'] == null) {
+                dict[id]['totalEpisodes'] = '?'
+            }
+            
+            ele.innerHTML = `
+                <div class="chart-square" style="background-image: url('${dict[id]['coverImage']}')">
+                    <div class="gradient">
+                        <p class="title">${dict[id]['title']}</p>
+                        <div class="sub">
+                            <p style="text-align: left">${secondsToString(dict[id]['timeUntilAiring'])}</p>
+                            <p style="text-align: right; filter: brightness(0.75)">${dict[id]['nextEpisode']}/${dict[id]['totalEpisodes']}</p>
+                        </div>
+                    </div>
+                </div>
+            `
+            grid.appendChild(ele)
+        }
+
+        // hide loading
+        loading.style.display = 'none'
     }
 }
 
-// at least it works...
+function secondsToString(s) {
+    let d = Math.floor(s / (3600 * 24))
+    let h = Math.floor(s % (3600 * 24) / 3600)
+    return `${d}d${h}h`
+}
 
 // TODO: option only watching list
